0d273cc35c9a1ce35b7d513ef9ccc582464d7c68
Dimitra <Dimitra@LAPTOP-J44K1LS3>
2023-02-18 11:27:02 +0200

    Add project files.

---
+++ convert/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace convert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            print();
+        }
+        static int sum(int a, int b)
+        {
+            return a + b; 
+        }
+        
+        static int substract(int a, int b)
+        {
+            return a - b;
+            
+        }
+        static int devide(int a, int b)
+        {
+            return a / b;
+           
+        }
+        static int multiply (int a, int b)
+        {
+            return a * b;
+            
+        }
+        static void print()
+        {
+            Console.WriteLine(sum(5, 2));
+            Console.WriteLine(multiply(2, 4));
+        }
+        
+    }
+}
